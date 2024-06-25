34f02d7dbcadbc9c8bd1229b6d8f68504db57c5a
uruwhy <58484522+uruwhy@users.noreply.github.com>
2024-06-24 18:36:47 -0700

    clean up macro code

--- winapihash/djb2macro/src/lib.rs
+++ winapihash/djb2macro/src/lib.rs
@@ -5,29 +5,14 @@ use quote::quote;
 use syn::{
     parse_macro_input,
     LitStr,
-    parse::{
-        Parse,
-        ParseStream,
-    },
 };
-
-struct Djb2MacroInput {
-    input_str: LitStr,
-}
-
-impl Parse for Djb2MacroInput {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        Ok(Self {
-            input_str: input.parse()?,
-        })
-    }
-}
 
 // Calculate DJB2 hash of string at compile time
 #[proc_macro]
 pub fn djb2(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as LitStr);
-    let string_to_hash = input.value();
+    println!("Invoked djb2 macro");
+    let parsed = parse_macro_input!(input as LitStr);
+    let string_to_hash = parsed.value();
     let hash = djb2_hash(string_to_hash.as_bytes());
     let hash_literal = Literal::u32_suffixed(hash);
 
