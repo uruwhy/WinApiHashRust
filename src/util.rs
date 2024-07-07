bd22d4e450a509c1d40ce29c7e2d04d2a421bfc4
uruwhy <58484522+uruwhy@users.noreply.github.com>
2024-07-06 18:54:57 -0700

    move out unnecessary pieces into separate repos

--- src/util.rs
+++
@@ -1,5 +0,0 @@
-// Convert rust string to wide Windows string
-// Reference: https://github.com/microsoft/windows-rs/issues/973
-pub fn to_wstring(value: &str) -> Vec<u16> {
-    return value.encode_utf16().chain([0u16]).collect::<Vec<u16>>();
-}
